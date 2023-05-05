837b4d1caba245d9b642b708c4232c6722447f5c
Antoine Alberti <aa@antoinealberti.com>
2023-05-05 13:03:13 +0200

    Press a few white keys, just to see

--- src/fifths/keyboard/Keyboard.tsx
+++ src/fifths/keyboard/Keyboard.tsx
@@ -3,11 +3,18 @@ import React, {FC} from "react";
 import "./Keyboard.css"
 
 interface KeyboardProps {
-    chordName:string
+    chordName: string
 }
 
-const Keyboard:FC<KeyboardProps> = ({chordName}) => {
-    return <img className="keyboard" src={keyboard} alt={`${chordName} on keyboard`}></img>;
+const Keyboard: FC<KeyboardProps> = ({chordName}) => {
+    /* White key x positions in pixels: 12 33 55 76 97 119 140 162 183 205 226 247 269 290 */
+    return <div style={{position:"relative"}}>
+        <img className="keyboard" src={keyboard} alt={`${chordName} on keyboard`}/>
+        <div style={{transform:"translate(-50%, -50%)", position: "absolute", top: "90px", left: "12px", width: "10px", height: "10px", background:"blue", borderRadius:"50%"}}/>
+        <div style={{transform:"translate(-50%, -50%)", position: "absolute", top: "90px", left: "33px", width: "10px", height: "10px", background:"blue", borderRadius:"50%"}}/>
+        <div style={{transform:"translate(-50%, -50%)", position: "absolute", top: "90px", left: "119px", width: "10px", height: "10px", background:"blue", borderRadius:"50%"}}/>
+        <div style={{transform:"translate(-50%, -50%)", position: "absolute", top: "90px", left: "290px", width: "10px", height: "10px", background:"blue", borderRadius:"50%"}}/>
+    </div>;
 };
 
 export default Keyboard
