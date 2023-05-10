706147b80b587ff576a5811a3ca86bd1fc3ffa88
Antoine Alberti <aa@antoinealberti.com>
2023-05-10 11:30:06 +0200

    CM scale

--- src/fifths/chords/MusicTheory.test.ts
+++ src/fifths/chords/MusicTheory.test.ts
@@ -1,32 +1,36 @@
-import {Chord, Note} from "./MusicTheory";
+import {Chord, Note, Scale} from "./MusicTheory";
 
-describe("chord resolver", () => {
-    test.each([
-        ["CM", ["C", "E", "G"]],
-        ["DbM", ["Db", "F", "Ab"]],
-        ["DM", ["D", "F#", "A"]],
-        ["EbM", ["Eb", "G", "Bb"]],
-        ["EM", ["E", "Ab", "B"]],
-        ["FM", ["F", "A", "C"]],
-        ["F#M", ["F#", "Bb", "Db"]],
-        ["GM", ["G", "B", "D"]],
-        ["AbM", ["Ab", "C", "Eb"]],
-        ["AM", ["A", "Db", "E"]],
-        ["BM", ["B", "Eb", "F#"]],
-        ["BbM", ["Bb", "D", "F"]],
-        ["Cm", ["C", "Eb", "G"]],
-        ["Dbm", ["Db", "E", "Ab"]],
-        ["Dm", ["D", "F", "A"]],
-        ["Ebm", ["Eb", "F#", "Bb"]],
-        ["Em", ["E", "G", "B"]],
-        ["Fm", ["F", "Ab", "C"]],
-        ["F#m", ["F#", "A", "Db"]],
-        ["Gm", ["G", "Bb", "D"]],
-        ["Abm", ["Ab", "B", "Eb"]],
-        ["Am", ["A", "C", "E"]],
-        ["Bbm", ["Bb", "Db", "F"]],
-        ["Bm", ["B", "D", "F#"]],
-    ])("Chord %s is made up of %s", (chordName, notes) => {
-        expect(new Chord(chordName).notes()).toEqual(notes.map(name => new Note(name)))
-    })
+test.each([
+    ["CM", ["C", "E", "G"]],
+    ["DbM", ["Db", "F", "Ab"]],
+    ["DM", ["D", "F#", "A"]],
+    ["EbM", ["Eb", "G", "Bb"]],
+    ["EM", ["E", "Ab", "B"]],
+    ["FM", ["F", "A", "C"]],
+    ["F#M", ["F#", "Bb", "Db"]],
+    ["GM", ["G", "B", "D"]],
+    ["AbM", ["Ab", "C", "Eb"]],
+    ["AM", ["A", "Db", "E"]],
+    ["BM", ["B", "Eb", "F#"]],
+    ["BbM", ["Bb", "D", "F"]],
+    ["Cm", ["C", "Eb", "G"]],
+    ["Dbm", ["Db", "E", "Ab"]],
+    ["Dm", ["D", "F", "A"]],
+    ["Ebm", ["Eb", "F#", "Bb"]],
+    ["Em", ["E", "G", "B"]],
+    ["Fm", ["F", "Ab", "C"]],
+    ["F#m", ["F#", "A", "Db"]],
+    ["Gm", ["G", "Bb", "D"]],
+    ["Abm", ["Ab", "B", "Eb"]],
+    ["Am", ["A", "C", "E"]],
+    ["Bbm", ["Bb", "Db", "F"]],
+    ["Bm", ["B", "D", "F#"]],
+])("Chord %s is made up of %s", (chordName, notes) => {
+    expect(new Chord(chordName).notes()).toEqual(notes.map(name => new Note(name)))
 })
+
+test.each([
+    ["CM", ["CM", "Dm", "Em", "FM", "GM", "Am", "Bmb5"]]
+])("Scale %s is made up of %s", (scaleName, chords) => {
+    expect(new Scale(scaleName).chords()).toEqual(chords.map(name => new Chord(name)))
+});
--- src/fifths/chords/MusicTheory.ts
+++ src/fifths/chords/MusicTheory.ts
@@ -1,8 +1,41 @@
+export class Scale {
+    readonly name: String;
+    constructor(name: String) {
+        this.name = name;
+    }
+
+    chords(): Chord[] {
+        const majorProgression = [
+            {semitones: 0, modifier: "M"},
+            {semitones: 2, modifier: "m"},
+            {semitones: 4, modifier: "m"},
+            {semitones: 5, modifier: "M"},
+            {semitones: 7, modifier: "M"},
+            {semitones: 9, modifier: "m"},
+            {semitones: 11, modifier: "mb5"},
+        ]
+        const firstNote = new Chord(this.name).root()
+        return majorProgression
+            .map(generator => this.toChord(firstNote, generator))
+    }
+
+    private toChord(note: Note, generator: { modifier: string; semitones: number }) {
+        return new Chord(note.plusSemitones(generator.semitones).name + generator.modifier);
+    }
+}
+
 export class Chord {
     readonly name: string;
 
     constructor(name: string) {
         this.name = name;
+    }
+
+    notes(): Note[] {
+        if (this.isMinor())
+            return [this.root(), this.root().flatThird(), this.root().fifth()];
+        else
+            return [this.root(), this.root().third(), this.root().fifth()];
     }
 
     private root() {
@@ -12,13 +45,6 @@ export class Chord {
 
     private isMinor() {
         return this.name.endsWith("m");
-    }
-
-    notes(): Note[] {
-        if (this.isMinor())
-            return [this.root(), this.root().flatThird(), this.root().fifth()];
-        else
-            return [this.root(), this.root().third(), this.root().fifth()];
     }
 }
 
