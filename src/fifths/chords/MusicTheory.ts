a5188980e27b909f129b724a39ea45bc3d2c52a4
Antoine Alberti <aa@antoinealberti.com>
2023-05-11 16:01:37 +0200

    Don't display keyboards when no scale is selected

--- src/fifths/chords/MusicTheory.ts
+++ src/fifths/chords/MusicTheory.ts
@@ -12,6 +12,7 @@ export class Scale {
     }
 
     chords(): Chord[] {
+        if (!this.name) return []
         const degreeI = new Chord(this.name);
         const generators:ChordGenerator[] = this.chordGenerators(degreeI);
         return generators
